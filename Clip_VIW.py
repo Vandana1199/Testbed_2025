6e34ee7d1aba6e660d3bcccfe2bc277311d76644
Vandana1199 <vandanadarapaneni7@gmail.com>
2025-04-09 11:19:27 -0500

    Update Clip_VIW.py

--- Clip_VIW.py
+++ Clip_VIW.py
@@ -668,7 +668,7 @@ upload_model_file = drive.CreateFile({
 })
 upload_model_file.SetContentFile(final_model_file)
 upload_model_file.Upload()
-print(f"✅ Final model file uploaded to Google Drive folder: {final_model_file}")
+print(f"✅ Height_VI's_Weather_Data file uploaded to Google Drive folder: {final_model_file}")
 
 # === Gmail API Scope and Functions ===
 import base64
@@ -741,7 +741,7 @@ receiver_emails = [
     'vdzfb@missouri.edu', 
 ]
 
-subject_success = '✅ Height_VI's_Weather_Data Output CSV File'
+subject_success = "✅ Height_VI's_Weather_Data Output CSV File"
 body_success = "Hi Team,\n\nPlease find attached file which had EMLID intergrated with PT data along side another file with Clipped height, VI's and Weather Data/PT and Remote sensing processing pipeline.\n\nBest regards,\n Team Testbed"
 
 subject_failure = '❌ Script Execution Failed'
