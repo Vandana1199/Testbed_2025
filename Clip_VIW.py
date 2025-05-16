9e0d791bd36eaca4213cfa592d2d320db766e9e9
Vandana1199 <vandanadarapaneni7@gmail.com>
2025-05-16 10:01:18 -0500

    Update Clip_VIW.py

--- Clip_VIW.py
+++ Clip_VIW.py
@@ -63,7 +63,7 @@ testbed_file = None
 
 pattern_emlid = re.compile(r'^EMLID_(\d+\.\d+\.\d+)\.csv$')
 pattern_pt = re.compile(r'^PT_(\d+\.\d+\.\d+)\.csv$')
-testbed_filename = 'Corners_5.5.25.csv'
+testbed_filename = 'TestBed_StripCorners.csv'
 # testbed_filename = "TestBed_PreHarvestCorners_4.15.25.csv"
 
 # Function to sort files by date in filename
@@ -163,7 +163,7 @@ merged_filtered.dropna()
 
 # # === 17. Read and process plot corners ===
 # Read the CSV file
-corners = pd.read_csv("Corners_5.5.25.csv").rename(columns={'POINT_X': 'x', 'POINT_Y': 'y'})
+corners = pd.read_csv("TestBed_StripCorners.csv").rename(columns={'POINT_X': 'x', 'POINT_Y': 'y'})
 # Rename the 'plot' column to 'Plot'
 corners.rename(columns={'PlotArea': 'Plot'}, inplace=True)
 # print(corners.head())
@@ -867,13 +867,13 @@ def send_email_gmail_api(subject, body_text, to_emails, attachment_paths=None):
 # === Final Email Execution ===
 receiver_emails = [
     "darapanenivandana1199@gmail.com",
-    "vdzfb@missouri.edu", 
-    "bernardocandido@missouri.edu",
-    "emh3d9@missouri.edu",
-    "ummbv@missouri.edu",
-    "rashmi.p.sharma@missouri.edu",
-    "bpbf25@mizzou.edu",
-    "kbn8m@missouri.edu"
+    # "vdzfb@missouri.edu", 
+    # "bernardocandido@missouri.edu",
+    # "emh3d9@missouri.edu",
+    # "ummbv@missouri.edu",
+    # "rashmi.p.sharma@missouri.edu",
+    # "bpbf25@mizzou.edu",
+    # "kbn8m@missouri.edu"
 ]
 
 subject_success = '✅ Harevst, Vis and weather data CSV File'
