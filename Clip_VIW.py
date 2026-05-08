2c2d96ad356e2c930a8193cb5244bf849d7d03bd
Vandana1199 <vandanadarapaneni7@gmail.com>
2026-05-07 20:42:46 -0500

    Update print statement from 'Hello' to 'Goodbye'

--- Clip_VIW.py
+++ Clip_VIW.py
@@ -130,7 +130,7 @@ GPS = GPS_raw.rename(columns={"longitude(deg)": "X", "latitude(deg)": "Y"})
 GPS = GPS[['X', 'Y', 'GPST']]
 
 # time_diff = timedelta(hours=6, minutes=0, seconds=24)  # CST
-time_diff = timedelta(hours=5, minutes=0, seconds=24)  # CDT
+time_diff = timedelta(hours=9, minutes=59, seconds=50)  # CDT
 
 PT_scaled = PT_raw.sort_values('time').copy()
 PT_scaled['n'] = range(1, len(PT_scaled) + 1)
