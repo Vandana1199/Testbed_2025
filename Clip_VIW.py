d643d0394391f88cabbda1793a383f5877040704
ajaygautam111 <ag2c2@missouri.edu>
2026-07-21 12:39:16 -0500

    Update Clip_VIW.py

--- Clip_VIW.py
+++ Clip_VIW.py
@@ -350,8 +350,7 @@ def fetch_and_process_farm_data(clipped_df):
     config.sh_client_id = CLIENT_ID
     config.sh_client_secret = CLIENT_SECRET
 
-    #collection_id = "af3a84ba-9504-4788-ad0a-dd4a9b03b225" #Change if the colelction id expires
-    collection_id = "76882050-493a-4724-ab34-af8d55318027" #Testbed collection till 2026
+    collection_id = "af3a84ba-9504-4788-ad0a-dd4a9b03b225" #Change if the colelction id expires
     PlanetScope_data_collection = DataCollection.define_byoc(collection_id)
 
     df['Coordinates'] = df['Coordinates'].apply(wkt.loads)
